c17339d3a9ad621bc7311b45748adb5ab401fbbd
Maxi Amodei <maxi_amodei@hotmail.com>
2021-06-12 19:26:51 -0300

    sign up view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+  before_action :config_devise_params, if: :devise_controller?
+
+  protected
+
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[
+                                        first_name
+                                        last_name
+                                        username
+                                        email
+                                        password
+                                        password_confirmation
+                                      ])
+  end
+
+  private
+
+  def layout_by_resource
+    devise_controller? ? 'session' : 'application'
+  end
 end
